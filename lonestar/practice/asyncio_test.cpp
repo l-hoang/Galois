ce62be5aeb4a6073b7d40d1a188511df11138d8f
Hochan Lee <hochan@utexas.edu>
2019-04-25 01:32:11 -0500

    Implement AIO with non-blocking and SIGNAL PROCESSING.
    - I tried callback methods but it did not work for single thread.
    - Callback is a global function and when it is called by one thread, aio_return does not work.
    - Therefore, I chose SIGNAL processing method. It works but requires busy waiting anyway.

--- lonestar/practice/asyncio_test.cpp
+++ lonestar/practice/asyncio_test.cpp
@@ -9,8 +9,28 @@
 #include <string.h>
 
 #define NO_THREADS 5
+#define SIG_AIO SIGRTMIN+1
 
 int fd;
+
+//void read_handler(sigval_t sigval)
+void read_handler(int s, siginfo_t * info, void *ctx)
+{
+    printf("AIO handler is called\n");
+
+    struct aiocb *req;
+
+    req = (struct aiocb *)info->si_value.sival_ptr;
+
+    if (aio_error (req) == 0) {
+        int ret = aio_return(req);
+        printf("results: %c\n", *(char *) req->aio_buf);
+    }
+    else {
+        printf("Fail to get return\n");
+    }
+}
+
 
 void *read_char(void *char_ptr)
 {
@@ -19,14 +39,29 @@ void *read_char(void *char_ptr)
     struct aiocb cb;
     bzero(&cb, sizeof(cb));
 
+    struct sigaction action;
+    action.sa_sigaction = read_handler;
+    action.sa_flags = SA_SIGINFO;
+    sigemptyset(&action.sa_mask);
+    sigaction(SIG_AIO, &action, NULL);
     size_t BUF_SIZE = sizeof(char);
+    //cb.aio_fildes = open("input", O_RDONLY);//fd;
     cb.aio_fildes = fd;
     cb.aio_buf = malloc(BUF_SIZE);
     cb.aio_nbytes = BUF_SIZE;
     cb.aio_offset = char_no*sizeof(char);
+    //cb.aio_sigevent.sigev_notify = SIGEV_THREAD;
+    cb.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+    cb.aio_sigevent.sigev_signo = SIG_AIO;
+    //cb.aio_sigevent.sigev_notify_function = read_handler;
+    cb.aio_sigevent.sigev_notify_attributes = NULL;
+    cb.aio_sigevent.sigev_value.sival_ptr = &cb;
+
+    printf("Thread! offset:%d\n", char_no*sizeof(char));
 
     int ret = aio_read(&cb);
 
+    /*
     while (aio_error(&cb) == EINPROGRESS) ;
     //cb.aio_sigevent.sigev_value.
     if ((ret = aio_return(&cb)) > 0) {
@@ -35,6 +70,7 @@ void *read_char(void *char_ptr)
     } else {
         fprintf(stderr, "Read fail\n");
     }
+    */
 }
 
 int main(void)
@@ -48,6 +84,8 @@ int main(void)
         pthread_create(&pth[i], NULL, read_char, (void *)i);
     }
 
+    // [NOTE] IT iS NECESSARY! So, blocking AIO is preferable!
+    for (int i = 0; i < 5; i ++) sleep(1);
     pthread_exit(NULL);
     return 0;
 }
